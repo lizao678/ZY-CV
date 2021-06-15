10dbec7628c15fe16626dbb8787d3be970836199
ppzhang <mzhangyik@.qq.com>
2021-06-15 17:40:01 +0800

    zy个人网页修复bug

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './css/main.scss'
+import './libs/js/jQuery-3.6.min.js'
+import './libs/js/jquery.fullPage.js'
+import './libs/css/jquery.fullPage.css'
+import $ from 'jquery'
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const htmlPlugin = new HtmlWebpackPlugin({ // 创建插件的实例对象
+ template: './src/index.html', // 指定要用到的模板文件
+ filename: 'index.html' // 指定生成的文件的名称，该文件存在于内存中，在目录中不显示
+})
 module.exports = {
     mode: 'development', // mode 用来指定构建模式，在开发阶段都指定为'development'
     entry: path.join(__dirname, './src/index.js'),
@@ -9,8 +14,10 @@ module.exports = {
             arrowFunction: false
         }
     },
+    plugins: [ htmlPlugin ], // plugins 数组是 webpack 打包期间会用到的一些插件列表
     module: {
         rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
             { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
             { 
                 test: /\.jpg|png|gif|bmp|ttf|eot|svg|woff|woff2$/, 
